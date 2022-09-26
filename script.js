2bef5de107c32cd745b460eceeea52c1beb0335d
HerzogVonWiesel <58423784+HerzogVonWiesel@users.noreply.github.com>
2022-09-26 20:36:55 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,89 @@
+let canvas;
+let ctx;
+let field;
+let w, h;
+let size;
+let columns;
+let rows;
+let noiseZ;
+
+function setup() {
+  size = 20;
+  noiseZ = 0;
+  canvas = document.querySelector("#canvas");
+  ctx = canvas.getContext("2d");
+  reset();
+  window.addEventListener("resize", reset);
+}
+
+function initField() {
+  field = new Array(columns);
+  for (let x = 0; x < columns; x++) {
+    field[x] = new Array(columns);
+    for (let y = 0; y < rows; y++) {
+      field[x][y] = [0, 0];
+    }
+  }
+}
+
+function calculateField() {
+  for (let x = 0; x < columns; x++) {
+    for (let y = 0; y < rows; y++) {
+      let angle = noise.simplex3(x / 50, y / 50, noiseZ) * Math.PI * 2;
+      let length = noise.simplex3(x / 50 + 40000, y / 50 + 40000, noiseZ);
+      field[x][y][0] = angle;
+      field[x][y][1] = Math.abs(length);
+    }
+  }
+}
+
+function reset() {
+  w = canvas.width = window.innerWidth;
+  h = canvas.height = window.innerHeight;
+  noise.seed(Math.random());
+  columns = Math.floor(w / size) + 1;
+  rows = Math.floor(h / size) + 1;
+  initField();
+}
+
+function draw() {
+  requestAnimationFrame(draw);
+  calculateField();
+  noiseZ += 0.002;
+  clear();
+  drawField();
+}
+
+function clear() {
+  ctx.fillStyle = "white";
+  ctx.fillRect(0, 0, w, h);
+}
+
+function drawField() {
+  for (let x = 0; x < columns; x++) {
+    for (let y = 0; y < rows; y++) {
+      let angle = field[x][y][0];
+      let length = field[x][y][1];
+      ctx.save();
+      ctx.translate(x * size, y * size);
+      ctx.rotate(angle);
+      ctx.strokeStyle = "black";
+      ctx.lineWidth = 1.0;
+      ctx.beginPath();
+      ctx.moveTo(0, 0);
+      ctx.lineTo(0, size * length + 10);
+      ctx.translate(0, size * length + 10);
+      ctx.rotate(10);
+      ctx.moveTo(0, 0);
+      ctx.lineTo(0, 10);
+      ctx.moveTo(0, 0);
+      ctx.rotate(-20);
+      ctx.lineTo(0, 10);
+      ctx.stroke();
+      ctx.restore();
+    }
+  }
+}
+
+setup();
+draw();
